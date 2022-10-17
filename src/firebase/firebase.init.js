739dd2e539afd7d7e1eaf11047d82b70c7c52107
aknathweb <aniknwu.cse@gmail.com>
2022-10-17 15:32:19 +0600

    link with firebase and create login and signup two component and route

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDrdPxcQRZe02vENkDB5oXn6aOFoq-ypB8",
+    authDomain: "ema-john-simple-9eddb.firebaseapp.com",
+    projectId: "ema-john-simple-9eddb",
+    storageBucket: "ema-john-simple-9eddb.appspot.com",
+    messagingSenderId: "125553194768",
+    appId: "1:125553194768:web:f5d436d751462e9c411795"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
